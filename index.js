84c969b052e9605c51c814bdf75131c2c8e88394
Hussain Shaik <66374088+Hussain29@users.noreply.github.com>
2021-10-02 21:07:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 app.use(express.static("public/"));
 
 app.get("", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html")); // Home Page Display
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/about.html"));
+  res.sendFile(path.join(__dirname, "public/about.html")); // About Page Display
 });
 app.get("/contact-me", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/contact-me.html"));
+  res.sendFile(path.join(__dirname, "public/contact-me.html")); // Contact Page Display
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log("Running!");
